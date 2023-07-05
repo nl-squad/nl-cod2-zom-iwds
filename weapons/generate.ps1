5081ceaf0dce0715001818e0adfaa3ede9afb729
Tomasz Rutkowski <rutkowski.tomasz.3@gmail.com>
2023-07-05 15:43:22 +0200

    Make generate for windows more verbose

--- weapons/generate.ps1
+++ weapons/generate.ps1
@@ -1,44 +1,52 @@
-# define csv file
+# Define csv file
 $csvfile = "weapons.csv"
 
-# initial line number
+# Initial line number
 $lineno = 0
 
-# read the file line by line
+# Read the file line by line
 Get-Content $csvfile | ForEach-Object {
-    # increment line number
+    # Increment line number
     $lineno++
 
-    # skip first line
+    # Skip first line
     if ($lineno -eq 1) {
+        Write-Host "Skipping header line"
         continue
     }
 
-    # separating column names, which are the properties
+    # Separating column names, which are the properties
     if ($lineno -eq 2) {
         $properties = $_ -split ';'
+        Write-Host "Reading properties: $properties"
         continue
     }
 
-    # separating data for each file
+    # Separating data for each file
     $data = $_ -split ';'
+    Write-Host "Reading data: $data"
 
     $filename = $data[0]
 
-    # define path
+    # Define path
     $filepath = "../iwds/zz.iwd/weapons/mp/$filename"
+    Write-Host "Filepath is: $filepath"
 
-    # iterate over properties and data
+    # Iterate over properties and data
     for ($pidx=1; $pidx -lt $data.Length; $pidx++) {
-        # extract property and value
+        # Extract property and value
         $property_string = $properties[$pidx]
         $value = $data[$pidx]
+        Write-Host "Property: $property_string"
+        Write-Host "Value: $value"
 
-        # multiple properties might be separated by ','
+        # Multiple properties might be separated by ','
         $multiple_properties = $property_string -split ','
+        Write-Host "Multiple properties: $multiple_properties"
 
         foreach ($property in $multiple_properties) {
-            # use regex to replace the string
+            Write-Host "Processing property: $property"
+            # Use regex to replace the string
             (Get-Content $filepath) |
             Foreach-Object { $_ -replace "(${property}\\\\)[^\\\\]*\\\\", "`${1}${value}\\" } |
             Set-Content $filepath
