8bfb640c9ff8498ab7acd29011a0fc24ed6b1065
Tomasz Rutkowski <rutkowski.tomasz.3@gmail.com>
2023-07-05 15:52:30 +0200

    Extra logging

--- weapons/generate.ps1
+++ weapons/generate.ps1
@@ -14,45 +14,39 @@ foreach($line in $lines) {
 
     # Skip first line
     if ($lineno -eq 1) {
-        Write-Host "Skipping header line"
         continue
     }
 
     # Separating column names, which are the properties
     if ($lineno -eq 2) {
         $properties = $line -split ';'
-        Write-Host "Reading properties: $properties"
         continue
     }
 
     # Separating data for each file
     $data = $line -split ';'
-    Write-Host "Reading data: $data"
 
     $filename = $data[0]
 
     # Define path
     $filepath = "../iwds/zz.iwd/weapons/mp/$filename"
-    Write-Host "Filepath is: $filepath"
 
     # Iterate over properties and data
     for ($pidx=1; $pidx -lt $data.Length; $pidx++) {
         # Extract property and value
         $property_string = $properties[$pidx]
         $value = $data[$pidx]
-        Write-Host "Property: $property_string"
-        Write-Host "Value: $value"
 
         # Multiple properties might be separated by ','
         $multiple_properties = $property_string -split ','
-        Write-Host "Multiple properties: $multiple_properties"
 
         foreach ($property in $multiple_properties) {
-            Write-Host "Processing property: $property"
+            Write-Host "Updating property '$property' with value '$value' in file '$filename'"
             # Use regex to replace the string
             (Get-Content $filepath) |
             Foreach-Object { $_ -replace "(${property}\\\\)[^\\\\]*\\\\", "`${1}${value}\\" } |
-            Set-Content $filepath
+            Out-File $filepath -Encoding utf8 -NoNewline
         }
     }
+    Write-Host "Processed: $filename"
 }
